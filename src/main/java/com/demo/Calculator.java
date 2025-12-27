1521bcd1641a50dd0dfd1adf2edcda98b957f927
Wissen <kavita.bandoliya@wissen.com>
2025-12-27 15:40:01 +0530

    Added mul and div methods

--- src/main/java/com/demo/Calculator.java
+++ src/main/java/com/demo/Calculator.java
@@ -9,10 +9,23 @@ public class Calculator {
     public int sub(int a, int b) {
         return a - b;
     }
+    
+    // task
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
 
     public static void main(String[] args) {
         Calculator c = new Calculator();
         System.out.println("Add: " + c.add(10, 5));
         System.out.println("Sub: " + c.sub(10, 5));
+        System.out.println("Mul: " + c.mul(10, 5));
+        System.out.println("Div: " + c.div(10, 5));
     }
+
 }
